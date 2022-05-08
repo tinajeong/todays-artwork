befa6421e75c882eb67ebb1b21dabfecc7eab95a
Tina <migujeong@gmail.com>
2022-05-08 21:50:58 +0900

    [fix] place img in center

--- src/component/Loading.tsx
+++ src/component/Loading.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import moai_spin from "../images/moai_spin.webp";
 export default function Loading() {
   return (
-    <div className="place-self-center">
+    <div className="grid grid-cols-1 place-items-center m-6">
       <img src={moai_spin} width={36} height={36} alt="spin"></img>
       <h3>Loading...</h3>
     </div>
